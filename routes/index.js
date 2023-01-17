d8c74eb13db5a95c830453d2f25d09483fdfbd70
benjofalanga <ansible@switzerland.fila>
2023-01-17 15:37:26 +0000

    show one price

--- routes/index.js
+++ routes/index.js
@@ -1,40 +1,40 @@
 var express = require('express');
+const { ConsoleMessage } = require('puppeteer');
 var router = express.Router();
 const lib=require('./results')
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
 
-  res.render('index')
+  res.render('index', { title: 'Search events '});
 });
 
+/* Post the returned events */
 router.post('/',async function(req,res)
 
   {   
     console.log('posted')
-    // res.sendStatus()
     response = {
-      first_name:req.body,
+      event_res:req.body,
     };
-    console.log(response.first_name.artist);
-    const artist=response.first_name.artist;
+    console.log(response.event_res.artist);
+    const artist=response.event_res.artist;
   console.log(artist)
   var fetch=await lib.art(artist)
   console.log(fetch)
-  var dict=[]
+  var eventlist=[]
   if (fetch.length)
   {
     for (i=0;i <fetch.length;i++){
-        dict.push(fetch[i])
+        eventlist.push(fetch[i])
+        
+        
     }
   }
-  console.log("Retuned",dict.length)
-  // res.render('index', { title: 'Search events ' ,events:dict});
-    // res.send(dict)
-   res.render('index', { title: 'Search events ' ,events:dict,band:artist});
-      // res.end(JSON.stringify(response));
+console.log("Returned",eventlist.length)
   
-  //     // res.render('index')
+   res.render('index', {title:'Found events',events:eventlist,band:artist});
+  
   }
  
 )
