b963408699a1308c166e33eac95bb7654ac0b44f
benjofalanga <ansible@switzerland.fila>
2023-01-17 15:39:40 +0000

    main func edit

--- routes/results.js
+++ routes/results.js
@@ -5,47 +5,64 @@ const pretty=require('pretty');
 const { ConsoleMessage } = require("puppeteer");
 
 
-// async function  get_events(artist){
+
   
   var fetch= (url) => axios.get(url)
-  .then((response) => {
+  .then(async (response) => {
     console.log("getting response")
     
-    const $ =cheerio.load(response.data)
-    // fs.writeFileSync('response.txt',pretty($.html()))
+    const $ =cheerio.load(await response.data)
+ 
     nu_of_events=$('#eventSelectionBox > div > div.filter.js-filter > div.listing-control.listing-control-in-card.margin-bottom-s.theme-element-border > div.listing-control-info.margin-bottom-m > span')
     int_nu_of_events=nu_of_events.text().split(' ')
     console.log("There are ",parseInt(int_nu_of_events[0])," events")
     events=$('.listing-item')
       let dict=[]
+      let saved=[]
 
 
     for (let i=0 ;i<events.length;i++){
+      console.log("event ",i)
       let month=$(events[i]).find(".event-listing-month")
       let day=$(events[i]).find(".event-listing-date")
       let time=$(events[i]).find(".event-listing-time")
       let city=$(events[i]).find('.event-listing-city')
       let price=$(events[i]).find('.event-price-available')
       let url_link=$(events[i]).find('.btn')
+      const regx=new RegExp(/(?<=ab)(.*?)(?=ab)/gs)
+      let result=async(price) => {
+        const res= regx.exec(await price)
+        return res
+        }
+      var res=await result(price.text())
+      if (res)
+      {
+        res=res[0]
+      }
+      else res='"unknown"'
+      // console.log(result.index)
       dict.push(
         {
           month:month.text(),
           day:day.text(),
           time:time.text(),
           city:city.text(),
-          price:price.text(),
+          
+          price: res ,
           url:"https://eventim.de"+url_link.attr('href')
         }
 
 
       )
+      console.log(dict[i])
     }
-    console.log(dict)
+    // console.log(dict)
     return dict
     })
   .catch((err) => 
-  // console.log("Fetch error " + err)
-  dict=[]
+    console.log(err)
+    // dict=[]
+
   )
   const printArtist = async (url) => {
     const a = await fetch(url);
@@ -56,12 +73,10 @@ const { ConsoleMessage } = require("puppeteer");
 
   async function art (artist){
     var url='https://www.eventim.de/artist/'+artist
-      var dict=await printArtist(url)
+      const dict = await fetch(url);
+    
+      
       return dict
     }
 
-  // art('metallica')
-
-  //   dict=art('kreator')
-    // console.log(dict.then((response)=>{return response}))
 module.exports = { art };
